76776b8f5cb9c31e1068c49b016f1546da834955
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 22:03:02 +0300

    Added UrlToImageConverter

---
+++ AnySoftMobile/AnySoftMobile/Utils/Converters/UrlToImageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using AnySoftMobile;
+using Xamarin.Forms;
+
+namespace AnySoftDesktop.Utils.Converters;
+
+public class UrlToImageConverter : IValueConverter
+{
+    public static UrlToImageConverter Instance { get; } = new();
+    
+    public object? Convert(object? value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        var path = (value ?? "").ToString();
+        if (path is null)
+            return "";
+        if (path.StartsWith("/"))
+            path = path.Substring(1);
+        return $"{App.CdnUrl}{path}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
