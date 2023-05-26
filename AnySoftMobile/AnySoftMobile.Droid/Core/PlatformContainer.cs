f55deb6adb53f3c7b49c7b4bfa774db8f223ed38
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 18:49:55 +0300

    Added platform application container

---
+++ AnySoftMobile/AnySoftMobile.Droid/Core/PlatformContainer.cs
@@ -0,0 +1,15 @@
+﻿using AnySoftMobile.Core;
+using Autofac;
+
+namespace AnySoftMobile.Droid.Core
+{
+    public class PlatformContainer : AppContainer
+    {
+        protected override void RegisterServices(ContainerBuilder containerBuilder)
+        {
+            base.RegisterServices(containerBuilder);
+
+            //Register platform-specific services
+        }
+    }
+}
