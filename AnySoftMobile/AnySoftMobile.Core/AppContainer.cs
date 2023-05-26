162eca800fe2646f3afeb7bb137416fd65b4dfff
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-26 18:48:20 +0300

    Added core application container

---
+++ AnySoftMobile/AnySoftMobile.Core/AppContainer.cs
@@ -0,0 +1,39 @@
+﻿using AnySoftMobile.Utils;
+using AnySoftMobile.Utils.Dialogs;
+using AnySoftMobile.ViewModels;
+using AnySoftMobile.Views;
+using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
+using Xamarin.Forms;
+
+namespace AnySoftMobile.Core
+{
+    public abstract class AppContainer
+    {
+        public void Setup()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            RegisterServices(containerBuilder);
+
+            var container = containerBuilder.Build();
+
+            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(container));
+
+            container.BeginLifetimeScope();
+        }
+
+        protected virtual void RegisterServices(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<App>().SingleInstance();
+
+            containerBuilder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
+            containerBuilder.RegisterType<JobDialogService>().As<IJobDialogService>().InstancePerDependency();
+
+            containerBuilder.RegisterType<MainView>().Named<Page>(ViewNames.MainView).As<MainView>().InstancePerDependency();
+            
+            containerBuilder.RegisterType<MainViewModel>().InstancePerDependency();
+        }
+    }
+}
