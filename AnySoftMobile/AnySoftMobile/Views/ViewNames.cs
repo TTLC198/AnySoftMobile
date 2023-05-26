907eefad3add9dee33c0aad1b88551c1fb68d655
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-27 00:32:47 +0300

    Added DashboardView to factory

--- AnySoftMobile/AnySoftMobile/Views/ViewNames.cs
+++ AnySoftMobile/AnySoftMobile/Views/ViewNames.cs
@@ -3,7 +3,7 @@
     public static class ViewNames
     {
         public const string MainView = nameof(Views.MainView);
-        
         public const string SingleProductView = nameof(Views.SingleProductView);
+        public const string DashboardView = nameof(Views.DashboardView);
     }
 }
