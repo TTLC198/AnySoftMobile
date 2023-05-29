cfc160837ec895c886b58ed1aa4ee803658a4ef1
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-29 15:52:02 +0300

    Added search model for better UX experience

---
+++ AnySoftMobile/AnySoftMobile/Models/SearchModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using AnySoftBackend.Library.DataTransferObjects.Product;
+
+namespace AnySoftMobile.Models;
+
+public class SearchModel
+{
+    public string SearchString { get; set; } = "";
+    public IEnumerable<ProductResponseDto> Products { get; set; } = new List<ProductResponseDto>();
+}
