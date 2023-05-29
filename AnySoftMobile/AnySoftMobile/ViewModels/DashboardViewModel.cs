c58d62ff3cc48bcae54af2ca4cc14702e5d4a6e1
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-29 15:50:57 +0300

    Added ability to search products

--- AnySoftMobile/AnySoftMobile/ViewModels/DashboardViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/DashboardViewModel.cs
@@ -5,10 +5,15 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Input;
 using AnySoftBackend.Library.DataTransferObjects.Product;
 using AnySoftDesktop.Utils;
+using AnySoftMobile.Models;
 using AnySoftMobile.Services;
 using AnySoftMobile.Utils.Dialogs;
+using AnySoftMobile.Views;
+using Xamarin.Forms;
 using XF.Material.Forms.UI.Dialogs;
 
 namespace AnySoftMobile.ViewModels;
@@ -17,22 +22,73 @@ public class DashboardViewModel : BaseViewModel
 {
     private readonly IJobDialogService _dialogService;
 
-    private ObservableCollection<ProductResponseDto> _products;
+    public ICommand OnSearchBarTextEntered { get; set; }
+
+    private ObservableCollection<ProductResponseDto> _products = new();
 
     public ObservableCollection<ProductResponseDto> Products
     {
         get => _products;
         set => Set(ref _products, value);
     }
+    
+    public string? SearchString { get; set; }
 
     public DashboardViewModel(IJobDialogService dialogService)
     {
         _dialogService = dialogService;
+        OnSearchBarTextEntered = new Command(async () => await OpenSearchPage());
     }
 
     public override async void OnViewAppearing(object sender, EventArgs args)
     {
         await UpdateProducts();
+    }
+    
+    public async Task OpenSearchPage()
+    {
+        try
+        {
+            var productRequestDto = new ProductRequestDto
+            {
+                Name = string.IsNullOrEmpty(SearchString)
+                    ? null
+                    : SearchString,
+                /*Genres = SelectedGenre is {Id: > 0}
+                    ? new List<int>() {SelectedGenre.Id}
+                    : null,
+                Properties = SelectedProperty is {Id: > 0}
+                    ? new List<int>() {SelectedProperty.Id}
+                    : null,*/
+            };
+            var productRequestQueryJson =
+                HttpUtility.UrlEncode(JsonSerializer.Serialize(productRequestDto, CustomJsonSerializerOptions.Options));
+            var getProductsRequest = await WebApiService.GetCall($"api/products?Query={productRequestQueryJson}");
+            if (getProductsRequest.IsSuccessStatusCode)
+            {
+                var timeoutAfter = TimeSpan.FromMilliseconds(3000);
+                using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                var responseStream = await getProductsRequest.Content.ReadAsStreamAsync();
+                var productsFiltered = await JsonSerializer.DeserializeAsync<IEnumerable<ProductResponseDto>>(responseStream,
+                    CustomJsonSerializerOptions.Options, cancellationToken: cancellationTokenSource.Token);
+                if (productsFiltered is not null)
+                    await Navigation.PushAsync(ViewNames.MultipleProductsView, new SearchModel()
+                    {
+                        SearchString = SearchString ?? "",
+                        Products = productsFiltered
+                    });
+
+            }
+            else
+            {
+                var msg = await getProductsRequest.Content.ReadAsStringAsync();
+                throw new InvalidOperationException($"{getProductsRequest.ReasonPhrase}\n{msg}");
+            }
+        }
+        catch (Exception exception)
+        {
+            await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+        }
     }
 
     private async Task UpdateProducts()
