3ed248581ecee0be563c5526cc45a680c804899c
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-29 14:14:21 +0300

    Added MultipleProductsView

---
+++ AnySoftMobile/AnySoftMobile/Views/MultipleProductsView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnySoftMobile.ViewModels;
+using AnySoftMobile.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnySoftMobile.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class MultipleProductsView : BaseMultipleProductsView
+{
+    public MultipleProductsView(DashboardViewModel dashboardViewModel)
+    {
+        InitializeComponent();
+    }
+}
+
+public abstract class BaseMultipleProductsView : BaseView<MultipleProductsViewModel>
+{
+}
