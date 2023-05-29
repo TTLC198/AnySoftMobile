ec2727f18bf963b71ba482499f457ee23356f477
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-29 15:52:21 +0300

    Added MultipleProductsView to ViewNames

--- AnySoftMobile/AnySoftMobile/Views/ViewNames.cs
+++ AnySoftMobile/AnySoftMobile/Views/ViewNames.cs
@@ -4,6 +4,7 @@
     {
         public const string MainView = nameof(Views.MainView);
         public const string SingleProductView = nameof(Views.SingleProductView);
+        public const string MultipleProductsView = nameof(Views.MultipleProductsView);
         public const string DashboardView = nameof(Views.DashboardView);
     }
 }
