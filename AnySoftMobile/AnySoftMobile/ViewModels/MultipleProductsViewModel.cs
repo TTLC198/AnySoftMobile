59343f48c1a3dc921856dabfcccf3e5b7cb4921a
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-29 15:51:36 +0300

    Added first iteration of main interface and logic of MultipleProductsView

--- AnySoftMobile/AnySoftMobile/ViewModels/MultipleProductsViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/MultipleProductsViewModel.cs
@@ -1,6 +1,39 @@
-﻿namespace AnySoftMobile.ViewModels;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AnySoftBackend.Library.DataTransferObjects.Product;
+using AnySoftMobile.Models;
+using AnySoftMobile.Utils.Dialogs;
+
+namespace AnySoftMobile.ViewModels;
 
 public class MultipleProductsViewModel : BaseViewModel
 {
+    private readonly IJobDialogService _dialogService;
     
+    private ObservableCollection<ProductResponseDto> _products = new ();
+
+    public ObservableCollection<ProductResponseDto> Products
+    {
+        get => _products;
+        set => Set(ref _products, value);
+    }
+
+    private string? _searchString;
+    
+    public string? SearchString { 
+        get => _searchString;
+        set => Set(ref _searchString, value); 
+    }
+
+    public MultipleProductsViewModel(IJobDialogService dialogService)
+    {
+        _dialogService = dialogService;
+    }
+    
+    public override void OnViewPushed(object navigationParameter = null!)
+    {
+        if (navigationParameter is not SearchModel searchModel) return;
+        SearchString = searchModel.SearchString;
+        Products = new ObservableCollection<ProductResponseDto>(searchModel.Products);
+    }
 }
--- AnySoftMobile/AnySoftMobile/Views/MultipleProductsView.xaml.cs
+++ AnySoftMobile/AnySoftMobile/Views/MultipleProductsView.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using AnySoftBackend.Library.DataTransferObjects.Product;
 using AnySoftMobile.ViewModels;
-using AnySoftMobile.Views;
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace AnySoftMobile.Views;
@@ -13,7 +8,7 @@ namespace AnySoftMobile.Views;
 [XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class MultipleProductsView : BaseMultipleProductsView
 {
-    public MultipleProductsView(DashboardViewModel dashboardViewModel)
+    public MultipleProductsView(MultipleProductsViewModel multipleProductsViewModel)
     {
         InitializeComponent();
     }
