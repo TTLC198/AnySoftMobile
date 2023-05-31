c9cd97bd7e4589443e778aaa1c3659a27c6fa12e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-31 16:47:50 +0300

    Changed MainView: added tabbed page

---
+++ AnySoftMobile/AnySoftMobile/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AnySoftMobile.Views;
+
+namespace AnySoftMobile.ViewModels;
+
+public class MainViewModel : BaseViewModel
+{
+    public override void OnViewAppearing(object sender, EventArgs args)
+    {
+    }
+}
---
+++ AnySoftMobile/AnySoftMobile/Views/MainView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnySoftMobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnySoftMobile.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class MainView : TabbedPage
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
