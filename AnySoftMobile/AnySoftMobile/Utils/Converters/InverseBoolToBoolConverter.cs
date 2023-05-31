b33e18c880aa4c55e0abe3c405b9c1e7cb69ec70
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-05-31 17:34:48 +0300

    Added converter

---
+++ AnySoftMobile/AnySoftMobile/Utils/Converters/InverseBoolToBoolConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AnySoftMobile.Utils.Converters;
+
+public class InverseBoolToBoolConverter : IValueConverter
+{
+    public static InverseBoolToBoolConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is not true;
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        value is not true;
+}
