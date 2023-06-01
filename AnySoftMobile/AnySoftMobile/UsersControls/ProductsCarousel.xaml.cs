c662bc3418f64f02ef893602e68468112a7a2c1e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-01 23:09:49 +0300

    Added ProductsCarousel

---
+++ AnySoftMobile/AnySoftMobile/UsersControls/ProductsCarousel.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnySoftBackend.Library.DataTransferObjects.Product;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnySoftMobile.UsersControls;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class ProductsCarousel : CarouselView
+{
+    public static readonly BindableProperty ProductsProperty =
+        BindableProperty.Create( nameof(Products), 
+            typeof(ObservableCollection<ProductResponseDto>),
+            typeof(ProductsCarousel), 
+            new ObservableCollection<ProductResponseDto>());
+
+    public ObservableCollection<ProductResponseDto> Products {
+        get => (ObservableCollection<ProductResponseDto>)GetValue(ProductsProperty);
+        set => SetValue(ProductsProperty, value );
+    }
+    
+    public ProductsCarousel()
+    {
+        InitializeComponent();
+    }
+}
