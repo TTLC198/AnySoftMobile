01c61d16bd7dc7118517b623dcc2345e3638fc50
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-01 23:07:21 +0300

    Added static VersionManager

---
+++ AnySoftMobile/AnySoftMobile/Utils/VersionManager.cs
@@ -0,0 +1,35 @@
+﻿using AnySoftDesktop.Models;
+using Xamarin.Forms;
+
+namespace AnySoftMobile.Utils;
+
+public class VersionManager : BindableObject
+{
+    private static readonly BindableProperty ApplicationUserProperty =
+        BindableProperty.Create( "ApplicationUser", 
+            typeof(ApplicationUser),
+            typeof(VersionManager), 
+            new ApplicationUser());
+
+    public ApplicationUser ApplicationUser {
+        get => (ApplicationUser)GetValue(ApplicationUserProperty);
+        set => SetValue(ApplicationUserProperty, value );
+    }
+    
+    private static readonly BindableProperty IsAuthorizedProperty =
+        BindableProperty.Create( "IsAuthorized", 
+            typeof(bool),
+            typeof(VersionManager), 
+            false);
+
+    public bool IsAuthorized {
+        get => (bool)GetValue(IsAuthorizedProperty);
+        set => SetValue(IsAuthorizedProperty, value );
+    }
+
+    public static VersionManager Instance { get; private set; }
+
+    static VersionManager() {
+        Instance = new VersionManager();
+    }
+}
