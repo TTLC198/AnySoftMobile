8456ef062102f01539c860fa565d08bee670dfd7
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-02 18:02:09 +0300

    Added first iteration of CartView

--- AnySoftMobile/AnySoftMobile/ViewModels/CartViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/CartViewModel.cs
@@ -1,10 +1,96 @@
-﻿using AnySoftMobile.Utils.Dialogs;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AnySoftBackend.Library.DataTransferObjects.Product;
+using AnySoftDesktop.Utils;
+using AnySoftMobile.Models;
+using AnySoftMobile.Services;
+using AnySoftMobile.Utils;
+using AnySoftMobile.Utils.Dialogs;
+using AnySoftMobile.Views;
+using Xamarin.Forms;
+using XF.Material.Forms.UI.Dialogs;
 
 namespace AnySoftMobile.ViewModels;
 
-public class CartViewModel : MultipleProductsViewModel
+public class CartViewModel : BaseViewModel
 {
-    public CartViewModel(IJobDialogService dialogService) : base(dialogService)
+    private readonly IJobDialogService _dialogService;
+
+    private ObservableCollection<ProductResponseDto> _products = new();
+
+    public ObservableCollection<ProductResponseDto> Products
     {
+        get => _products;
+        set => Set(ref _products, value);
+    }
+
+    public ICommand OnProductViewEntered { get; set; }
+
+    public CartViewModel(IJobDialogService dialogService)
+    {
+        _dialogService = dialogService;
+        OnProductViewEntered = new Command(OpenSingleProductPage);
+    }
+
+    private async void OpenSingleProductPage(object id)
+    {
+        await Navigation.PushAsync(ViewNames.SingleProductView, id);
+    }
+
+    public override async void OnViewPushed(object navigationParameter = null!)
+    {
+        await UpdateProductsInCart();
+    }
+
+    public override async void OnViewAppearing(object sender, EventArgs args)
+    {
+        await UpdateProductsInCart();
+    }
+
+    private async Task UpdateProductsInCart()
+    {
+        try
+        {
+            var getProductsRequest =
+                await WebApiService.GetCall($"api/cart", VersionManager.Instance.ApplicationUser.JwtToken!);
+            if (getProductsRequest.IsSuccessStatusCode)
+            {
+                var timeoutAfter = TimeSpan.FromMilliseconds(3000);
+                using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                var responseStream = await getProductsRequest.Content.ReadAsStreamAsync();
+                var products = await JsonSerializer.DeserializeAsync<IEnumerable<ProductResponseDto>>(responseStream,
+                    CustomJsonSerializerOptions.Options, cancellationToken: cancellationTokenSource.Token);
+                if (products is not null)
+                    Products = new ObservableCollection<ProductResponseDto>(products
+                        .Select(p => new ProductResponseDto()
+                        {
+                            Id = p.Id,
+                            Name = p.Name,
+                            Cost = p.Cost,
+                            Discount = p.Discount,
+                            Rating = p.Rating,
+                            Seller = p.Seller,
+                            Description = p.Description?.Substring(0, 45) + "...",
+                            Images = p.Images,
+                            Genres = p.Genres,
+                            Properties = p.Properties
+                        }));
+            }
+            else
+            {
+                var msg = await getProductsRequest.Content.ReadAsStringAsync();
+                throw new InvalidOperationException($"{getProductsRequest.ReasonPhrase}\n{msg}");
+            }
+        }
+        catch (Exception exception)
+        {
+            await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+        }
     }
 }
