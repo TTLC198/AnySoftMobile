cc757bc7c366fcc8c397229be171a24aa5e35937
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-02 15:46:27 +0300

    Added first iteration of ProfileViewModel

--- AnySoftMobile/AnySoftMobile/ViewModels/ProfileViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/ProfileViewModel.cs
@@ -1,6 +1,57 @@
-﻿namespace AnySoftMobile.ViewModels;
+﻿using System.Collections.ObjectModel;
+using AnySoftBackend.Library.DataTransferObjects.Order;
+using AnySoftDesktop.Models;
+
+namespace AnySoftMobile.ViewModels;
 
 public class ProfileViewModel : BaseViewModel
 {
+    private ApplicationUser _applicationUser;
+    public ApplicationUser ApplicationUser
+    {
+        get => _applicationUser;
+        set
+        {
+            _applicationUser = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private ObservableCollection<CustomPayment> _paymentMethods = new ObservableCollection<CustomPayment>();
+
     
+    public ObservableCollection<CustomPayment> PaymentMethods
+    {
+        get => _paymentMethods;
+        set
+        {
+            _paymentMethods = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private ObservableCollection<OrderResponseDto> _orders = new ObservableCollection<OrderResponseDto>();
+
+    
+    public ObservableCollection<OrderResponseDto> Orders
+    {
+        get => _orders;
+        set
+        {
+            _orders = value;
+            OnPropertyChanged();
+        }
+    }
+    
+    private string? _userImagePath;
+
+    public string? UserImagePath
+    {
+        get => _userImagePath;
+        set
+        {
+            _userImagePath = value;
+            OnPropertyChanged();
+        }
+    }
 }
