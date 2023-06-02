69357407d87baf0f8a55c5c3eca8cfcfce93fd80
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-02 18:54:05 +0300

    Added ability to add single product to shopping cart.

--- AnySoftMobile/AnySoftMobile/ViewModels/SingleProductViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/SingleProductViewModel.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AnySoftBackend.Library.DataTransferObjects.Order;
 using AnySoftBackend.Library.DataTransferObjects.Product;
 using AnySoftBackend.Library.DataTransferObjects.Review;
+using AnySoftBackend.Library.DataTransferObjects.ShoppingCart;
 using AnySoftBackend.Library.DataTransferObjects.User;
 using AnySoftDesktop.Utils;
 using AnySoftMobile.Services;
 using AnySoftMobile.Utils;
+using Xamarin.Forms;
 using XF.Material.Forms.UI.Dialogs;
 
 namespace AnySoftMobile.ViewModels
@@ -40,7 +43,7 @@ namespace AnySoftMobile.ViewModels
             get => _isBought;
             set => Set(ref _isBought, value);
         }
-        
+
         public ReviewCreateDto? NewReview { get; set; } = new();
 
         private bool _isReviewAdded;
@@ -50,7 +53,7 @@ namespace AnySoftMobile.ViewModels
             get => _isReviewAdded;
             set => Set(ref _isReviewAdded, value);
         }
-    
+
         private bool _isReviewModified;
 
         public bool IsReviewModified
@@ -58,7 +61,7 @@ namespace AnySoftMobile.ViewModels
             get => _isReviewModified;
             set => Set(ref _isReviewModified, value);
         }
-    
+
         private bool _isOwnReviewExists;
 
         public bool IsOwnReviewExists
@@ -67,10 +70,17 @@ namespace AnySoftMobile.ViewModels
             set => Set(ref _isOwnReviewExists, value);
         }
 
+        public ICommand OnAddToCartButtonClicked { get; set; }
+
+        public SingleProductViewModel()
+        {
+            OnAddToCartButtonClicked = new Command(OnCartButtonClick);
+        }
+
         public override async void OnViewAppearing(object sender, EventArgs args)
         {
         }
-        
+
         public override async void OnViewPushed(object navigationParameter = null!)
         {
             if (navigationParameter is not int productId) return;
@@ -94,14 +104,16 @@ namespace AnySoftMobile.ViewModels
                         if (product is null)
                             throw new InvalidOperationException("Product is null");
                         product.Reviews?
-                            .Where(r => (r.User ?? new UserResponseDto()).Id == VersionManager.Instance.ApplicationUser?.Id)
+                            .Where(r => (r.User ?? new UserResponseDto()).Id ==
+                                        VersionManager.Instance.ApplicationUser?.Id)
                             .ToList()
                             .ForEach(r => r.IsOwn = true);
                         IsOwnReviewExists = product.Reviews?.Any(r => r.IsOwn) ?? false;
                         Product = product;
                     }
 
-                    var getOrdersRequest = await WebApiService.GetCall("api/orders", VersionManager.Instance.ApplicationUser?.JwtToken!);
+                    var getOrdersRequest = await WebApiService.GetCall("api/orders",
+                        VersionManager.Instance.ApplicationUser?.JwtToken!);
                     if (getOrdersRequest.IsSuccessStatusCode)
                     {
                         using (var cancellationTokenSource = new CancellationTokenSource(timeoutAfter))
@@ -138,6 +150,85 @@ namespace AnySoftMobile.ViewModels
                 {
                     var msg = await getProductsRequest.Content.ReadAsStringAsync();
                     throw new InvalidOperationException($"{getProductsRequest.ReasonPhrase}\n{msg}");
+                }
+            }
+            catch (Exception exception)
+            {
+                await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+            }
+        }
+
+        public async void OnCartButtonClick(object idObject)
+        {
+            try
+            {
+                if (idObject is not int id)
+                    return;
+                if (IsBought)
+                    return;
+                if (IsInCart)
+                    await OnRemoveFromCartButtonClick(id);
+                else
+                    await OnAddToCartButtonClick(id);
+            }
+            catch (Exception exception)
+            {
+                await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+            }
+        }
+
+        private async Task OnAddToCartButtonClick(int id)
+        {
+            var jsonObject = new
+            {
+                productIds = new List<int>()
+                {
+                    id
+                }
+            };
+            var postProductsToCartRequest =
+                await WebApiService.PostCall("api/cart", jsonObject, VersionManager.Instance.ApplicationUser.JwtToken!);
+            try
+            {
+                if (postProductsToCartRequest.IsSuccessStatusCode)
+                {
+                    var timeoutAfter = TimeSpan.FromMilliseconds(3000);
+                    using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                    var responseStream =
+                        await postProductsToCartRequest.Content.ReadAsStreamAsync();
+                    var cart = await JsonSerializer.DeserializeAsync<ShoppingCartResponseDto>(responseStream,
+                        CustomJsonSerializerOptions.Options, cancellationToken: cancellationTokenSource.Token);
+                    if (cart.Products.Any(p => p.Id == Product.Id))
+                    {
+                        IsInCart = true;
+                    }
+                }
+                else
+                {
+                    var msg = await postProductsToCartRequest.Content.ReadAsStringAsync();
+                    throw new InvalidOperationException($"{postProductsToCartRequest.ReasonPhrase}\n{msg}");
+                }
+            }
+            catch (Exception exception)
+            {
+                await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+            }
+        }
+
+        private async Task OnRemoveFromCartButtonClick(int id)
+        {
+            var removeProductsFromCartRequest =
+                await WebApiService.DeleteCall($"api/cart?productId={id}", VersionManager.Instance.ApplicationUser.JwtToken!);
+            try
+            {
+                if (removeProductsFromCartRequest.IsSuccessStatusCode)
+                {
+                    IsInCart = false;
+                }
+                else
+                {
+                    var msg = await removeProductsFromCartRequest.Content.ReadAsStringAsync();
+                    throw new InvalidOperationException($"{removeProductsFromCartRequest.ReasonPhrase}\n{msg}");
                 }
             }
             catch (Exception exception)
