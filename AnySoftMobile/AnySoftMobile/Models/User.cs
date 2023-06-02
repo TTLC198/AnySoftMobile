88dd5358625915e7a539eeda1acda4cf70105709
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-02 18:53:11 +0300

    Added local user entity

---
+++ AnySoftMobile/AnySoftMobile/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AnySoftMobile.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+        public string? Email { get; set; }
+        public int RoleId { get; set; }
+    }
+}
