509db867f05f397a5a3ddedc983804222db7bcfe
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-02 17:32:12 +0300

    Added CartView

---
+++ AnySoftMobile/AnySoftMobile/ViewModels/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AnySoftMobile.Utils.Dialogs;
+
+namespace AnySoftMobile.ViewModels;
+
+public class CartViewModel : MultipleProductsViewModel
+{
+    public CartViewModel(IJobDialogService dialogService) : base(dialogService)
+    {
+    }
+}
---
+++ AnySoftMobile/AnySoftMobile/Views/CartView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnySoftMobile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnySoftMobile.Views;
+
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class CartView : BaseCartView
+{
+    public CartView(CartViewModel cartViewModel)
+    {
+        InitializeComponent();
+    }
+
+    public CartView()
+    {
+        InitializeComponent();
+    }
+}
+
+public abstract class BaseCartView : BaseView<CartViewModel>
+{
+}
