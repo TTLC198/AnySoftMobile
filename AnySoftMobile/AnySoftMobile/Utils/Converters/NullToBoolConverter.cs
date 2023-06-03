e83781a2b4b0c965497aa249ae63e4bee4c36293
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-03 22:31:21 +0300

    Added specific converters

---
+++ AnySoftMobile/AnySoftMobile/Utils/Converters/NullToBoolConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AnySoftMobile.Utils.Converters;
+
+public class NullToBoolConverter : IValueConverter
+{
+    public static NullToBoolConverter Instance { get; } = new();
+    
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is not null;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class InverseNullToBoolConverter : IValueConverter
+{
+    public static InverseNullToBoolConverter Instance { get; } = new();
+    
+    public object Convert(object? value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value is null ? true : false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AnySoftMobile/AnySoftMobile/Utils/Converters/OrderStatusToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace AnySoftMobile.Utils.Converters;
+
+public class OrderStatusToBoolConverter : IValueConverter
+{
+    public static OrderStatusToBoolConverter Instance { get; } = new();
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return 
+            value is not (string and "Paid");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+        throw new NotImplementedException();
+}
