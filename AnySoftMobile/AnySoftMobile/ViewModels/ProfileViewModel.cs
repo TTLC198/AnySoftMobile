2c2a399ec54285982281a4cd4ab667227a49329e
TTLC198 <41226242+TTLC198@users.noreply.github.com>
2023-06-03 18:29:07 +0300

    Added payment CRUD main logic

--- AnySoftMobile/AnySoftMobile/ViewModels/ProfileViewModel.cs
+++ AnySoftMobile/AnySoftMobile/ViewModels/ProfileViewModel.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Input;
 using AnySoftBackend.Library.DataTransferObjects.Order;
+using AnySoftBackend.Library.DataTransferObjects.Payment;
 using AnySoftBackend.Library.DataTransferObjects.Product;
 using AnySoftBackend.Library.DataTransferObjects.User;
 using AnySoftDesktop.Models;
@@ -79,9 +80,21 @@ public class ProfileViewModel : BaseViewModel
     public ICommand OnProfileChangedCommand { get; set; }
     
     public ICommand OnLogoutClickedCommand { get; set; }
+    
+    public ICommand OnAddPaymentClickedCommand { get; set; }
+    
+    public ICommand OnSavePaymentClickedCommand { get; set; }
+    
+    public ICommand OnRemovePaymentClickedCommand { get; set; }
+    
+    public ICommand OnEditPaymentClickedCommand { get; set; }
 
     public ProfileViewModel()
     {
+        OnAddPaymentClickedCommand = new Command(AddPayment);
+        OnSavePaymentClickedCommand = new Command(SavePayment);
+        OnRemovePaymentClickedCommand = new Command(RemovePayment);
+        OnEditPaymentClickedCommand = new Command(EditPayment);
         OnProfileChangedCommand = new Command(OnProfileChanged);
         OnLogoutClickedCommand = new Command(OnLogoutClicked);
     }
@@ -174,6 +187,131 @@ public class ProfileViewModel : BaseViewModel
             {
                 var msg = await putChangesOfUserRequest.Content.ReadAsStringAsync();
                 throw new InvalidOperationException($"{putChangesOfUserRequest.ReasonPhrase}\n{msg}");
+            }
+        }
+        catch (Exception exception)
+        {
+            await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+        }
+    }
+
+    private async void AddPayment()
+    {
+        PaymentMethods.Add(new CustomPayment() { CardName = "", Number = "", Cvc = "", IsEditComplete = true });
+    }
+
+    private async void SavePayment(object paymentObject)
+    {
+        try
+        {
+            if (paymentObject is not CustomPayment payment) return;
+            var paymentDto = new PaymentCreateDto()
+            {
+                CardName = payment.CardName,
+                Number = payment.Number,
+                ExpirationDate = new DateTime(payment.ExpirationDate!.Year, payment.ExpirationDate.Month, 1),
+                Cvc = payment.Cvc
+            };
+            var postPaymentMethodRequest =
+                await WebApiService.PostCall("api/payment", paymentDto, VersionManager.Instance.ApplicationUser.JwtToken!);
+            if (postPaymentMethodRequest.IsSuccessStatusCode)
+            {
+                await UpdatePayments();
+            }
+            else
+            {
+                var msg = await postPaymentMethodRequest.Content.ReadAsStringAsync();
+                throw new InvalidOperationException($"{postPaymentMethodRequest.ReasonPhrase}\n{msg}");
+            }
+        }
+        catch (Exception exception)
+        {
+            await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+        }
+    }
+
+    private async void EditPayment(object paymentObject)
+    {
+        if (paymentObject is not CustomPayment payment) return;
+        if (payment.IsEditComplete)
+        {
+            try
+            {
+                var paymentEditDto = new PaymentEditDto()
+                {
+                    Id = payment.Id.Value,
+                    Number = payment.Number,
+                    CardName = payment.CardName,
+                    ExpirationDate = new DateTime(payment.ExpirationDate!.Year, payment.ExpirationDate.Month, 1),
+                    Cvc = payment.Cvc
+                };
+                var putPaymentRequest =
+                    await WebApiService.PutCall($"api/payment", paymentEditDto, VersionManager.Instance.ApplicationUser.JwtToken!);
+                if (putPaymentRequest.IsSuccessStatusCode)
+                {
+                    var timeoutAfter = TimeSpan.FromMilliseconds(3000);
+                    using var cancellationTokenSource = new CancellationTokenSource(timeoutAfter);
+                    var responseStream =
+                        await putPaymentRequest.Content.ReadAsStreamAsync();
+                    var editedPayment = await JsonSerializer.DeserializeAsync<Payment>(
+                        responseStream,
+                        CustomJsonSerializerOptions.Options,
+                        cancellationToken: cancellationTokenSource.Token);
+                    var existedPayment = PaymentMethods.FirstOrDefault(p => p.Id == payment.Id);
+                    if (editedPayment is null)
+                        throw new InvalidOperationException("Existed payment does not exist");
+                    if (existedPayment is null)
+                        throw new InvalidOperationException("Existed payment does not exist");
+                    existedPayment.IsEditComplete = false;
+                    existedPayment.Number = editedPayment.Number;
+                    existedPayment.CardName = editedPayment.CardName;
+                    existedPayment.ExpirationDate = new CustomDate()
+                        {Month = editedPayment.ExpirationDate.Month, Year = editedPayment.ExpirationDate.Year};
+                    existedPayment.Cvc = editedPayment.Cvc;
+                }
+                else
+                {
+                    var msg = await putPaymentRequest.Content.ReadAsStringAsync();
+                    throw new InvalidOperationException($"{putPaymentRequest.ReasonPhrase}\n{msg}");
+                }
+            }
+            catch (Exception exception)
+            {
+                await MaterialDialog.Instance.AlertAsync(message: $"{exception.Message}".Trim());
+            }
+        }
+        else
+        {
+            var existedPayment = PaymentMethods.FirstOrDefault(pm => pm.Id == payment.Id);
+            if (existedPayment is not null)
+                existedPayment.IsEditComplete = true;
+        }
+    }
+
+    private async void RemovePayment(object idObject)
+    {
+        try
+        {
+            if (idObject is not int id) return;
+            if (id == 0)
+            {
+                await UpdatePayments();
+                return;
+            }
+
+            var postProductsToCartRequest =
+                await WebApiService.DeleteCall($"api/payment/{id}", VersionManager.Instance.ApplicationUser.JwtToken!);
+            if (postProductsToCartRequest.IsSuccessStatusCode)
+            {
+                var paymentToDelete = PaymentMethods.FirstOrDefault(p => p.Id == id);
+                if (paymentToDelete is null)
+                    throw new InvalidOperationException("Payment not found");
+                PaymentMethods.Remove(paymentToDelete);
+            }
+            else
+            {
+                var msg = await postProductsToCartRequest.Content.ReadAsStringAsync();
+                throw new InvalidOperationException($"{postProductsToCartRequest.ReasonPhrase}\n{msg}");
             }
         }
         catch (Exception exception)
